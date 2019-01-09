652506d0ba125d09761c421a4c0f9f344985d2d1
Jesse Liberty <jesseliberty@gmail.com>
2019-01-09 09:10:09 -0500

    Initial commit of base view model

---
+++ BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Demo3.ViewModel
+{
+   public abstract class BaseViewModel : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      public void OnPropertyChanged( string propertyName )
+      {
+         PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+      }
+
+      protected void OnPropChanged( [CallerMemberName] string propertyName = null )
+      {
+         PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+      }
+
+      protected void SetValue<T>( ref T backingField,
+         T value,
+         [CallerMemberName] string propertyName = null )
+      {
+         if (EqualityComparer<T>.Default.Equals( backingField, value ))
+         {
+            return;
+         }
+
+         backingField = value;
+         OnPropChanged( propertyName );
+      }
+   }
+}
